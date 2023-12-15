e565372ba9333a573d562fff153f65eee6bac490
nissysunny2023 <153511291+nissysunny2023@users.noreply.github.com>
2023-12-15 10:59:59 +0530

    njh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DistrictComponent } from './pages/district/district.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavbarComponent,LoginComponent,DistrictComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
